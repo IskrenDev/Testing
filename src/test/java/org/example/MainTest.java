f5253e120be0bc76de5b23aea6bab03801e1598a
iskrenr <iskrenr005@gmail.com>
2023-09-20 15:41:43 +0200

    Tests schreiben

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void sum_whenNumber1PlusNumber2_thenReturnSum() {
+        //GIVEN
+        int number1 = 10;
+        int number2 = 10;
+        int expected = 20;
+        //WHEN
+        int actual = Main.sum(number1, number2);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void sum_whenNumber1NotEquals20_thenExpectAssertionError() {
+        //GIVEN
+        int number1 = 10;
+        int number2 = 10;
+        int expected = 20;
+        //WHEN
+        int actual = Main.sum(number1, number2);
+        //THEN
+        assertEquals(expected, actual);
+
+    }
+
+}
